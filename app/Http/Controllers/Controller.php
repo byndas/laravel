908de868b25c40e44904b3a5973b3a712e8d11a0
lemokh <byndasma@gmail.com>
2021-04-04 23:26:37 +0400

    config database.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 // use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 // use Illuminate\Foundation\Bus\DispatchesJobs;
 // use Illuminate\Foundation\Validation\ValidatesRequests;
-// use Illuminate\Routing\Controller as BaseController;
+// use Illuminate\Routing\Controller as Controller;
 
-class Controller extends BaseController
+class Controller extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function home()
