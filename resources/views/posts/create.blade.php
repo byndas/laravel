d873276a0df77c4b797efe6fb118c0b377cb28fd
lemokh <byndasma@gmail.com>
2021-04-11 22:26:46 +0400

    create.blade..php --actions comment

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,9 +3,11 @@
 @section('title', 'Create the post')
 
 @section('content')
+{{-- don't ever create actions that save, modify, or delete data on behalf of the user using the GET http verb --}}
 <form action="{{ route('posts.store') }}" method="POST">
     @csrf
     @include('posts.partials.form')
     <div><input type="submit" value="Create" class="btn btn-primary btn-block"></div>
+
 </form>
 @endsection
